e03221b7f0dc658c05d3c2cbe31bc9163adc93c1
aousabdo <aousabdo@gmail.com>
2015-03-31 16:17:11 -0400

    Added main figure

--- server.R
+++ server.R
@@ -9,6 +9,16 @@ library(shiny)
 
 shinyServer(function(input, output) {
 
+  pop <- reactive({
+    DT <- simPop(N = input$N, n = input$n, a = sqrt(input$size), b = sqrt(input$size))
+    return(DT)
+  })
+  
+  output$encHist <- renderPlot({
+    tmp <- totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move)
+    hist(tmp, col = 'darkgray', border = 'white', main = "Distribution of Number of Encounters", xlab = "Number of Encounters")
+  })
+  
   output$distPlot <- renderPlot({
 
     # generate bins based on input$bins from ui.R
--- ui.R
+++ ui.R
@@ -1,30 +1,33 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
-
+  
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
+  titlePanel("Simulating Random Encounters in Population"),
+  
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      wellPanel(
+        sliderInput("N", "Total Number of Participants in Conference:", min = 10000, max = 100000, value = 40000, step = 10000), 
+        br(),
+        sliderInput("n", "Number of Active Personas:", min = 10, max = 200, value = 160, step = 10),
+        br(),
+        sliderInput("size", "Conference Area in sq ft.:", min = 100000, max = 3000000, value = 2600000, step = 100000),
+        br(),
+        sliderInput("iter", "Total Number of Hours to Simulate:", min = 1, max = 24, value = 8, step = 1),
+        br(),
+        sliderInput("x0", "Beacon Range (ft.):", min = 5, max = 100, value = 50, step = 5),
+        br(),
+        sliderInput("p_Move", "Percentage of population Actively Moving Around", min = 0.2, max = 1, value = 0.5, step = 0.1)
+        )
+      
     ),
-
+    
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("encHist")
     )
   )
 ))
