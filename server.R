9431ec28a6c398e207abb310f9d3b1722bee97bd
aousabdo <aousabdo@gmail.com>
2015-04-01 15:03:17 -0400

    Added more text outputs, in sentence formats

--- server.R
+++ server.R
@@ -3,26 +3,40 @@ library(shiny)
 shinyServer(function(input, output) {
   
   pop <- reactive({
+    input$goButton
     DT <- simPop(N = input$N, n = input$n, a = sqrt(input$size), b = sqrt(input$size))
     return(DT)
   })
   
   output$encHist <- renderPlot({
-    tmp <<- totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move)
+    input$goButton
+    tmp <- isolate(totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move))
     Histogram <<- hist(tmp, col = 'steelblue', border = 'white', main = "Distribution of Number of Encounters", 
-                       xlab = "Number of Encounters")
+                       xlab = "Number of Encounters", cex.lab = 1.5, cex.axis = 1.5, cex.main = 1.5 )
     #     abline(v = median(tmp), col = "red", lwd = 3)
     #     abline(v = median(tmp[tmp != 0]), col = "blue", lwd = 3)
   })
   
-  output$text1 <- renderUI({
-    tmp <<- totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move)
+  output$text0 <- renderUI({
+    input$goButton
+    tmp <- isolate(totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move))
+    text0 <- isolate(paste0("For a conference taking place in a square area of ", input$size,  " sq ft., where ", input$N, 
+                   " people are present and out of which ", input$n, " are active participants, if we assume a beacon 
+                   range of ", input$x0, " feet and we assume that ", 100*input$p_Move, "% of the total population 
+                   moves each hour, then for a total period of ", input$iter, " hours, we expect that, on average, each of our ",
+                   input$n, " personas would have ", median(tmp)," encounters"))
+    HTML(text0)
+  })
+    output$text1 <- renderUI({
+    input$goButton
+    tmp <- isolate(totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move))
     text1 <- paste("Median: ", median(tmp), sep = " ")
     HTML(text1)
   })
   
   output$text2 <- renderUI({
-    tmp <<- totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move)
+    input$goButton
+    tmp <- isolate(totalEnc(DT = pop(), x0 = input$x0, iter = input$iter, p_Move = input$p_Move))
     text2 <- paste("Median (excluding zero encounters): ", median(tmp[tmp != 0]), sep = " ")
     HTML(text2)
   })
--- ui.R
+++ ui.R
@@ -13,25 +13,29 @@ shinyUI(fluidPage(
         sliderInput("N", "Total Number of Participants in Conference:", min = 10000, max = 100000, value = 40000, step = 10000), 
         br(),
         sliderInput("n", "Number of Active Personas:", min = 10, max = 200, value = 160, step = 10),
-        br(),
+        tags$hr(),
         sliderInput("size", "Conference Area in sq ft.:", min = 100000, max = 3000000, value = 2600000, step = 100000),
         br(),
         sliderInput("iter", "Total Number of Hours to Simulate:", min = 1, max = 24, value = 4, step = 1),
         br(),
         sliderInput("x0", "Beacon Range (ft.):", min = 5, max = 100, value = 50, step = 5),
+        tags$hr(),
+        sliderInput("p_Move", "Percentage of population Actively Moving Around", min = 0.2, max = 1, value = 0.5, step = 0.1), 
         br(),
-        sliderInput("p_Move", "Percentage of population Actively Moving Around", min = 0.2, max = 1, value = 0.5, step = 0.1)
+        actionButton("goButton", "Run!")
         )
       
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      tags$style(type='text/css', '#text1 {background-color: rgba(0,0,0,0); color: red; font-size: 22px}'),
-      tags$style(type='text/css', '#text2 {background-color: rgba(0,0,0,0); color: blue;font-size: 22px}'),
-      plotOutput("encHist"), 
-      htmlOutput("text1"),
-      htmlOutput("text2")
+      tags$style(type='text/css', '#text0 {background-color: rgba(0,0,0,0); color: blue; font-size: 22px}'),
+      # tags$style(type='text/css', '#text1 {background-color: rgba(0,0,0,0); color: red; font-size: 22px}'),
+      # tags$style(type='text/css', '#text2 {background-color: rgba(0,0,0,0); color: blue;font-size: 22px}'),
+      plotOutput("encHist", width = "800px", height = "600px"), 
+      htmlOutput("text0")
+#       htmlOutput("text1"),
+#       htmlOutput("text2")
     )
   )
 ))
